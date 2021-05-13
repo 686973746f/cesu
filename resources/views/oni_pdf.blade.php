52cc3fbdeba7ba85dafb4ea6b98252abcc3eb45b
hihihisto <hihihisto@protonmail.com>
2021-05-13 18:07:15 +0800

    fix oni print mk.1

--- resources/views/oni_pdf.blade.php
+++ resources/views/oni_pdf.blade.php
@@ -11,12 +11,12 @@
     <div class="container-fluid" style="font-family: Arial, Helvetica, sans-serif; page-break-after: {{($n+11 < $list->count()) ? 'always;' : 'avoid'}};">
         <div class="text-center">
             <img src="{{asset('assets/images/oni_head.png')}}" alt="" style="width: 50rem;" class="{{($n+1 != 1) ? 'mt-3' : ''}}">
-            <h6 class="font-weight-bold mb-1">SAMPLE PICK-UP FORM</h6>
+            <h6 class="font-weight-bold mt-1 mb-1">SAMPLE PICK-UP FORM</h6>
         </div>
         <div class="row justify-content-center">
             <div class="col-md-4">
                 <span>Name of Institute/Facility: <u>{{$details->dru}}</u></span>
-                <p>Name and Number of Contact Person: <u>{{$details->contactPerson." ".$details->contactMobile}}</u></p>
+                <p class="mb-1">Name and Number of Contact Person: <u>{{$details->contactPerson." ".$details->contactMobile}}</u></p>
             </div>
         </div>
         <div class="table-responsive">
