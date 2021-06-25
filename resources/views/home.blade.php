93aa65572ec0528d740645240a76c8e507e6c42d
hihihisto <hihihisto@protonmail.com>
2021-06-25 11:43:14 +0800

    added fontawesome icons

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -22,13 +22,13 @@
                     <a href="{{route('records.index')}}" class="btn btn-primary btn-lg btn-block"><i class="fa fa-user mr-2" aria-hidden="true"></i>Patient Information</a>
                     <a href="{{route('forms.index')}}" class="btn btn-primary btn-lg btn-block"><i class="fa fa-file mr-2" aria-hidden="true"></i>View/Create CIF</a>
                     @if(is_null(auth()->user()->brgy_id) && is_null(auth()->user()->company_id))
-                    <a href="{{route('linelist.index')}}" class="btn btn-primary btn-lg btn-block">Line List</a>
+                    <a href="{{route('linelist.index')}}" class="btn btn-primary btn-lg btn-block"><i class="fas fa-archive mr-2"></i>Line List</a>
                     @endif
                     <hr>
                     <a href="{{route('report.index')}}" class="btn btn-primary btn-lg btn-block"><i class="fas fa-chart-bar mr-2"></i>Reports <i class="text-warning font-weight-bold">[Under Development]</i></a>
                     @if(auth()->user()->isAdmin == 1)
                         <hr>
-                        <a href="{{route('adminpanel.index')}}" class="btn btn-primary btn-lg btn-block">Admin Panel</a>
+                        <a href="{{route('adminpanel.index')}}" class="btn btn-primary btn-lg btn-block"><i class="fas fa-user-cog mr-2"></i>Admin Panel</a>
                     @endif
                 </div>
             </div>
