ffce845a9d0d3815021ddbccde8f52db87c3a3cf
hihihisto <hihihisto@protonmail.com>
2021-06-25 22:53:59 +0800

    added pa-swab

---
+++ resources/views/paswab_index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+    <form action="" method="POST">
+        <div class="card">
+            <div class="card-body">
+                
+            </div>
+        </div>
+    </form>
+@endsection
