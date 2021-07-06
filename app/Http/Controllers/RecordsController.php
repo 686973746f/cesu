b21eac26ba38c6fc503e42654ef3215a2376b17a
hihihisto <hihihisto@protonmail.com>
2021-07-06 11:19:34 +0800

    double entry fix v2

--- app/Http/Controllers/RecordsController.php
+++ app/Http/Controllers/RecordsController.php
@@ -499,9 +499,9 @@ class RecordsController extends Controller
 			}
 		}
 
-		$newmname = (!is_null($request->mname)) ? mb_strtoupper($request->mname) : NULL;
+		$newmname = ($request->filled('mname')) ? mb_strtoupper($request->mname) : NULL;
 
-		if($current->lname == mb_strtoupper($request->lname) && $current->fname == mb_strtoupper($request->lname) && $current->mname == $newmname && $current->bdate == $request->bdate && $current->gender == strtoupper($request->gender)) {
+		if($current->lname == mb_strtoupper($request->lname) && $current->fname == mb_strtoupper($request->fname) && $current->mname == $newmname && $current->bdate == $request->bdate && $current->gender == strtoupper($request->gender)) {
 			$param1 = 0;
 			$param2 = 0;
 		}
