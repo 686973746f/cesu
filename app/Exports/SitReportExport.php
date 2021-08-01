2c963153171280349ab050a07aa974171b118719
hihihisto <hihihisto@protonmail.com>
2021-08-01 17:17:31 +0800

    situational report temp

--- app/Exports/SitReportExport.php
+++ app/Exports/SitReportExport.php
@@ -2,6 +2,8 @@
 
 namespace App\Exports;
 
+use DateTime;
+use DateInterval;
 use App\Models\Forms;
 use Maatwebsite\Excel\Concerns\FromArray;
 use Maatwebsite\Excel\Concerns\WithTitle;
@@ -22,10 +24,146 @@ class SitReportExport implements WithMultipleSheets
     {
         $sheets = [];
 
+        $sheets[] = new ActiveConfirmedDistributionSheet();
+        $sheets[] = new ConfirmedVsNewCasesDistribution();
         $sheets[] = new SexDistributionSheet();
         $sheets[] = new AgeDistributionSheet();
+        $sheets[] = new NatureOfWorkDistributionSheet();
 
         return $sheets;
+    }
+}
+
+class ActiveConfirmedDistributionSheet implements FromArray, WithMapping, WithHeadings, WithTitle, ShouldAutoSize {
+    public function __construct()
+    {
+        $this->totalActiveCount = 0;
+        $this->dateArrays = array();
+
+        $startDate = new DateTime(date('Ymd', strtotime(Forms::with('records')
+        ->where('outcomeCondition', 'Active')
+        ->where('caseClassification', 'Confirmed')
+        ->orderBy('testDateCollected1', 'ASC')
+        ->pluck('testDateCollected1')
+        ->first())));
+        
+        $iterateDate = $startDate;
+        
+        $endDate = new DateTime(date('Ymd'));
+
+        while ($iterateDate <= $endDate) {
+            array_push($this->dateArrays, $iterateDate->format('Y-m-d'));
+
+            $iterateDate->modify('+1 Day');
+        }
+    }
+
+    public function array(): array
+    {
+        return $this->dateArrays;
+    }
+
+    public function title(): string
+    {
+        return 'Active Cases Distribution';
+    }
+
+    public function map($form): array {
+        $this->totalActiveCount += Forms::where('outcomeCondition', 'Active')
+        ->where('caseClassification', 'Confirmed')
+        ->where(function ($query) use ($form) {
+            $query->where('testDateCollected1', $form)
+            ->orWhere('testDateCollected2', $form);
+        })->count();
+
+        $recoveredCount = Forms::where('outcomeCondition', 'Recovered')
+        ->where(function ($query) use ($form) {
+            $query->where('testDateCollected1', $form)
+            ->orWhere('testDateCollected2', $form);
+        })->count();
+        
+        $totalCounts = $this->totalActiveCount - $recoveredCount;
+
+        return [
+            $form,
+            $totalCounts,
+        ];
+    }
+
+    public function headings(): array {
+        return [
+            '',
+            '',
+        ];
+    }
+}
+
+class ConfirmedVsNewCasesDistribution implements FromArray, WithMapping, WithHeadings, WithTitle, ShouldAutoSize {
+    public function __construct()
+    {
+        $this->totalActiveCount = 0;
+        $this->dateArrays = array();
+
+        $startDate = new DateTime(date('Ymd', strtotime(Forms::with('records')
+        ->where('outcomeCondition', 'Active')
+        ->where('caseClassification', 'Confirmed')
+        ->orderBy('testDateCollected1', 'ASC')
+        ->pluck('testDateCollected1')
+        ->first())));
+        
+        $iterateDate = $startDate;
+        
+        $endDate = new DateTime(date('Ymd'));
+
+        while ($iterateDate <= $endDate) {
+            array_push($this->dateArrays, $iterateDate->format('Y-m-d'));
+
+            $iterateDate->modify('+1 Day');
+        }
+    }
+
+    public function array(): array
+    {
+        return $this->dateArrays;
+    }
+
+    public function title(): string
+    {
+        return 'Confirmed vs. New Cases Distribution';
+    }
+
+    public function map($form): array {
+        $this->totalActiveCount += Forms::where('outcomeCondition', 'Active')
+        ->where('caseClassification', 'Confirmed')
+        ->where(function ($query) use ($form) {
+            $query->where('testDateCollected1', $form)
+            ->orWhere('testDateCollected2', $form);
+        })->count();
+
+        $recoveredCount = Forms::where('outcomeCondition', 'Recovered')
+        ->where(function ($query) use ($form) {
+            $query->where('testDateCollected1', $form)
+            ->orWhere('testDateCollected2', $form);
+        })->count();
+
+        $newCasesCount = Forms::where('outcomeCondition', 'Active')
+        ->where(function ($query) use ($form) {
+            $query->where('testDateCollected1', $form)
+            ->orWhere('testDateCollected2', $form);
+        })->count();
+
+        return [
+            $form,
+            ($this->totalActiveCount - $recoveredCount),
+            ($newCasesCount),
+        ];
+    }
+
+    public function headings(): array {
+        return [
+            '',
+            '',
+        ];
     }
 }
 
@@ -144,25 +282,66 @@ class AgeDistributionSheet implements FromArray, WithMapping, WithHeadings, With
     }
 }
 
-/*
-class SexDistributionSheet implements FromCollection, WithMapping, WithHeadings, WithTitle, ShouldAutoSize, WithStyles {
-    public function collection()
+class NatureOfWorkDistributionSheet implements FromArray, WithMapping, WithHeadings, WithTitle, ShouldAutoSize {
+    public function __construct()
     {
-        return Forms::whereIn('id', $this->id)
-        ->where('caseClassification', 'Suspect')
-        ->orderby('testDateCollected1', 'asc')
-        ->orderby('testDateCollected2', 'asc')
-        ->get();
+        $forms = Forms::with('records')
+        ->where('outcomeCondition', 'Active')
+        ->where('caseClassification', 'Confirmed')->get();
+
+        $this->recs = $forms->unique('records.natureOfWork')->pluck('records.natureOfWork')->toArray();
+        $this->activeCount = $forms->count();
     }
 
-    public function styles(Worksheet $sheet)
+    public function array(): array
     {
-        $sheet->getStyle(1)->getFont()->setBold(true);
+        return $this->recs;
     }
 
     public function title(): string
     {
-        return 'Sex Distribution';
+        return 'Nature of Work';
+    }
+
+    public function map($form): array {
+        return [
+            (!is_null($form)) ? $form : 'NON-WORKING',
+            Forms::with('records')
+            ->where('outcomeCondition', 'Active')
+            ->where('caseClassification', 'Confirmed')
+            ->whereHas('records', function ($query) use ($form) {
+                $query->where('records.natureOfWork', $form);
+            })->count(),
+        ];
+    }
+
+    public function headings(): array {
+        return [
+            '',
+            'Distribution of '.$this->activeCount.' Active Cases by Nature of Work'
+        ];
+    }
+}
+
+/*
+class NatureOfWorkDistributionSheet implements FromArray, WithMapping, WithHeadings, WithTitle, ShouldAutoSize {
+    public function array(): array
+    {
+        $forms = Forms::with('records')
+        ->where('outcomeCondition', 'Active')
+        ->where('caseClassification', 'Confirmed')->get();
+
+        $recs = $forms->unique('records.natureOfWork');
+
+        return [
+            NULL,
+            $recs,
+        ];
+    }
+
+    public function title(): string
+    {
+        return 'Nature of Work';
     }
 
     public function map($form): array {
