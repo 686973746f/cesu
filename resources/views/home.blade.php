71d002af3952c5b1968b1b5d88fe072ef9be268b
hihihisto <hihihisto@protonmail.com>
2021-09-02 08:09:35 +0800

    home buttons fixed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -26,7 +26,9 @@
                     <div class="collapse" id="collapseExample">
                         <div class="card card-body">
                             <a href="{{route('forms.index')}}" class="btn btn-primary btn-lg btn-block">View/Create CIFs</a>
+                            @if(auth()->user()->isCesuAccount())
                             <a href="{{route('paswab.view')}}" class="btn btn-primary btn-lg btn-block">Pa-swab List <span class="badge badge-light ml-1">{{number_format($paswabctr)}}</span></a>
+                            @endif
                             <a href="{{route('bulkupdate.index')}}" class="btn btn-primary btn-lg btn-block">Bulk Update CIF Status</a>
                         </div>
                     </div>
