9336067895c2c724f178275c7750ce6ed7af4cf6
Histo <hihihisto@gmail.com>
2021-09-27 15:42:20 +0800

    disabled auto recovered sa mga nasa facility

--- app/Console/Commands/AutoRecoveredActiveCases.php
+++ app/Console/Commands/AutoRecoveredActiveCases.php
@@ -46,40 +46,42 @@ class AutoRecoveredActiveCases extends Command
         $dateToday = Carbon::parse(date('Y-m-d'));
 
         foreach($forms as $item) {
-            if(!is_null($item->testDateCollected2)) {
-                $swabDateCollected = $item->testDateCollected2;
-            }
-            else {
-                $swabDateCollected = $item->testDateCollected1;
-            }
-
-            if($item->pType == 'PROBABLE' || $item->pType == 'TESTING') {
-                $startDate = Carbon::parse(date('Y-m-d', strtotime($swabDateCollected.' + 1 Day')));
-            }
-            else if($item->pType == 'CLOSE CONTACT') {
-                if(!is_null($item->expoitem1)) {
-                    $startDate = Carbon::parse(date('Y-m-d', strtotime($item->expoitem1)));
+            if($item->dispoType != 2) {
+                if(!is_null($item->testDateCollected2)) {
+                    $swabDateCollected = $item->testDateCollected2;
                 }
                 else {
+                    $swabDateCollected = $item->testDateCollected1;
+                }
+    
+                if($item->pType == 'PROBABLE' || $item->pType == 'TESTING') {
                     $startDate = Carbon::parse(date('Y-m-d', strtotime($swabDateCollected.' + 1 Day')));
                 }
-            }
-            
-            if($item->dispoType == 1 || $item->healthStatus == 'Severe' || $item->healthStatus == 'Critical') {
-                $daysToRecover = 21;
-            }
-            else {
-                $daysToRecover = 10;
-            }
-
-            $diff = $startDate->diffInDays($dateToday);
-            if($diff >= $daysToRecover) {
-                $update = Forms::find($item->id);
-
-                $update->outcomeCondition = 'Recovered';
-                $update->outcomeRecovDate = date('Y-m-d');
-
-                $update->save();
+                else if($item->pType == 'CLOSE CONTACT') {
+                    if(!is_null($item->expoitem1)) {
+                        $startDate = Carbon::parse(date('Y-m-d', strtotime($item->expoitem1)));
+                    }
+                    else {
+                        $startDate = Carbon::parse(date('Y-m-d', strtotime($swabDateCollected.' + 1 Day')));
+                    }
+                }
+                
+                if($item->dispoType == 1 || $item->healthStatus == 'Severe' || $item->healthStatus == 'Critical') {
+                    $daysToRecover = 21;
+                }
+                else {
+                    $daysToRecover = 10;
+                }
+    
+                $diff = $startDate->diffInDays($dateToday);
+                if($diff >= $daysToRecover) {
+                    $update = Forms::find($item->id);
+    
+                    $update->outcomeCondition = 'Recovered';
+                    $update->outcomeRecovDate = date('Y-m-d');
+    
+                    $update->save();
+                }
             }
         }
     }
