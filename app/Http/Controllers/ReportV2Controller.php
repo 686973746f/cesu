7d3d5902af887682f10df86d3af7ff71f097557d
Histo <hihihisto@gmail.com>
2021-09-28 15:43:26 +0800

    converter

--- app/Http/Controllers/ReportV2Controller.php
+++ app/Http/Controllers/ReportV2Controller.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use App\Models\Forms;
 use Illuminate\Http\Request;
 
@@ -52,5 +53,38 @@ class ReportV2Controller extends Controller
             ->count(),
             'death_list' => $death,
         ]);
+    }
+
+    public function convertFalseDate() {
+        $list = Forms::whereDate('dateReported', '>', date('Y-m-d'))->get();
+
+        foreach($list as $item) {
+            $i = Forms::find($item->id);
+            $switch = date('Y-d-m', strtotime($i->dateReported));
+            $i->dateReported = date('Y-m-d 08:00:00', strtotime($switch));
+            $i->created_at = date('Y-m-d 08:00:00', strtotime($switch));
+            $i->updated_at = date('Y-m-d 08:00:00', strtotime($switch));
+
+            if(Carbon::parse($i->testDateCollected1) > Carbon::now()) {
+                $switch2 = date('Y-d-m', strtotime($i->testDateCollected1));
+                $i->testDateCollected1 = date('Y-m-d', strtotime($switch2));
+            }
+            
+            if(!is_null($i->dateOnsetOfIllness)) {
+                if(Carbon::parse($i->dateOnsetOfIllness) > Carbon::now()) {
+                    $switch3 = date('Y-d-m', strtotime($i->dateOnsetOfIllness));
+                    $i->dateOnsetOfIllness = date('Y-m-d', strtotime($switch3));
+                }
+            }
+            
+            if(!is_null($i->dispoDate)) {
+                if(Carbon::parse($i->dispoDate) > Carbon::now()) {
+                    $switch4 = date('Y-d-m', strtotime($i->dispoDate));
+                    $i->dispoDate = date('Y-m-d 08:00:00', strtotime($switch4));
+                }
+            }
+
+            $i->save();
+        }
     }
 }
