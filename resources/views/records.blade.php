a3215f78fea11520477a7b046ca99b045e44b5a3
Histo <hihihisto@gmail.com>
2021-10-03 10:51:53 +0800

    failsafe fix

--- resources/views/records.blade.php
+++ resources/views/records.blade.php
@@ -67,6 +67,7 @@
                                 <th style="vertical-align: middle">Case Classification</th>
                                 <th style="vertical-align: middle">Encoded/Edited By</th>
                                 <th style="vertical-align: middle">Date Encoded/Edited</th>
+                                <th></th>
                             </tr>
                         </thead>
                         <tbody>
@@ -82,8 +83,8 @@
                                     <td style="vertical-align: middle" class="text-center">{{(!is_null($record->philhealth)) ? $record->philhealth : "N/A"}}</td>
                                     <td style="vertical-align: middle" class="text-center">{{(!is_null($record->occupation)) ? $record->occupation : "N/A"}}</td>
                                     <td style="vertical-align: middle"><small>{{$record->getAddress()}}</small></td>
-                                    <td style="vertical-align: middle" class="text-center">{{$record->form->pType}}</td>
-                                    <td style="vertical-align: middle" class="text-center">{{$record->form->pType}}</td>
+                                    <td style="vertical-align: middle" class="text-center">{{$record->form?->pType}}</td>
+                                    <td style="vertical-align: middle" class="text-center">{{$record->form?->caseClassification}}</td>
                                     <td style="vertical-align: middle" class="text-center">{{$record->user->name}}{{(!is_null($record->updated_by) && $record->user_id != $record->updated_by) ? ' / '.$record->getEditedBy() : ''}}</td>
                                     <td style="vertical-align: middle" class="text-center">{{(!is_null($record->updated_by)) ? date('m/d/Y h:i A', strtotime($record->updated_at)) : date('m/d/Y h:i A', strtotime($record->created_at))}}</td>
                                 </tr>
