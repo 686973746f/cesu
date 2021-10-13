68a571172a14c52eb007c83dc3321c8d092fd93d
hihihisto <hihihisto@protonmail.com>
2021-10-13 15:01:03 +0800

    added paswab icons

--- resources/views/paswab_language.blade.php
+++ resources/views/paswab_language.blade.php
@@ -4,6 +4,9 @@
     <div class="container" style="font-family: Arial, Helvetica, sans-serif">
         <div class="card">
             <div class="card-header text-center">
+                <img src="{{asset('assets/images/cho_icon_large.png')}}" style="width: 10rem;" class="mb-3">
+                <img src="{{asset('assets/images/gentri_icon_large.png')}}" style="width: 10rem;" class="mb-3">
+                <hr>
                 <h5>Province of Cavite</h5>
                 <h5>City of General Trias</h5>
                 <h5>City Health Office</h5>
@@ -15,8 +18,14 @@
                 <p>Select Language</p>
                 <p>Pumili ng Wika</p>
                 <hr>
-                <a class="btn btn-primary btn-block" href="{{route('paswab.index', ['locale' => 'en'])}}?rlink={{request()->input('rlink')}}&s={{request()->input('s')}}" role="button">English</a>
-                <a class="btn btn-primary btn-block" href="{{route('paswab.index', ['locale' => 'fil'])}}?rlink={{request()->input('rlink')}}&s={{request()->input('s')}}" role="button">Filipino / Tag-lish</a>
+                <div class="row">
+                    <div class="col-md-6">
+                        <a class="btn btn-primary btn-lg btn-block" href="{{route('paswab.index', ['locale' => 'en'])}}?rlink={{request()->input('rlink')}}&s={{request()->input('s')}}" role="button"><span class="flag-icon flag-icon-gb mr-2"></span>English</a>
+                    </div>
+                    <div class="col-md-6">
+                        <a class="btn btn-primary btn-lg btn-block" href="{{route('paswab.index', ['locale' => 'fil'])}}?rlink={{request()->input('rlink')}}&s={{request()->input('s')}}" role="button"><span class="flag-icon flag-icon-ph mr-2"></span>Filipino / Tag-lish</a>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
