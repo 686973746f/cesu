0262da43ed17d934255487452a7684de8fe8f211
hihihisto <hihihisto@protonmail.com>
2022-01-10 12:16:49 +0800

    allow for antigen paswab

--- resources/views/paswab_index.blade.php
+++ resources/views/paswab_index.blade.php
@@ -80,10 +80,12 @@
                                     <div class="form-group">
                                         <label for="forAntigen"><span class="text-danger font-weight-bold">*</span>{{__('paswab.forAntigen')}}</label>
                                         <select class="form-control" name="forAntigen" id="forAntigen" required>
+                                            <option value="" disabled {{is_null(old('forAntigen')) ? 'selected' : ''}}>{{__('paswab.select.Choose')}}</option>
                                             <option value="0" {{(old('forAntigen') == '0') ? 'selected' : ''}}>{{__('paswab.select.ChooseNo')}}</option>
+                                            <option value="1" {{(old('forAntigen') == '1') ? 'selected' : ''}}>{{__('paswab.select.ChooseYes')}}</option>
                                         </select>
                                         <small class="text-muted">{{__('paswab.forAntigenNotice')}}</small>
-                                        <small class="text-danger">Selecting "YES" for antigen is temporarily disabled. All Patients are suggested to take RT-PCR Test.</small>
+                                        <!--<small class="text-danger">Selecting "YES" for antigen is temporarily disabled. All Patients are suggested to take RT-PCR Test.</small>-->
                                     </div>
                                 </div>
                             </div>
