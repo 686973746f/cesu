24965baa3b069467aa41780cc338de06b8e1c7e7
Histo <hihihisto@gmail.com>
2022-04-03 00:13:08 +0800

    kernel changes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -29,7 +29,7 @@ class Kernel extends ConsoleKernel
         $schedule->command('autorecoveredactivecases:daily')->dailyAt('06:00')->evenInMaintenanceMode();
         $schedule->command('dailycaseslogging:daily')->dailyAt('13:00')->evenInMaintenanceMode();
         $schedule->command('dailycaseslogging:daily')->dailyAt('16:00')->evenInMaintenanceMode();
-        $schedule->command('autoemailreport:daily')->everyMinute();
+        $schedule->command('autoemailreport:daily')->everyMinute()->evenInMaintenanceMode();
         $schedule->command('autoemailreport:daily')->dailyAt('16:10')->evenInMaintenanceMode();
 
         //$schedule->command('test:everyminute')->everyMinute();
