f5e597e4d59a0ae2abb5e767513bac66ab48884e
Histo <hihihisto@gmail.com>
2022-04-02 23:48:36 +0800

    added covid report automated

---
+++ app/Mail/CovidReportWord.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class CovidReportWord extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('email.covidreport')
+        ->from('histoemulator@gmail.com', 'Christian James Historillo')
+        ->subject('COVID Gentri '.date('F d, Y'))
+        ->attach(asset('CITY-OF-GENERAL-TRIAS.docx'));
+    }
+}
---
+++ resources/views/email/covidreport.blade.php
@@ -0,0 +1,6 @@
+<div>
+    <p>Please see the attached file for the City of General Trias COVID-19 Case Bulletin.</p>
+    <p>As of {{date('F d, Y')}}</p>
+    <p>-</p>
+    <p>Note: Computer Generated file, Do Not Reply. Made possible by Christian James Historillo.</p>
+</div>
