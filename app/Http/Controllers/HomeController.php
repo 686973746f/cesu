180e0d39fcf3fa0a8928530efcaeb3feace914cc
Histo <hihihisto@gmail.com>
2022-04-05 19:04:36 +0800

    testmail test

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers;
 
 use Carbon\Carbon;
 use App\Models\Forms;
+use App\Mail\TestMail;
 use Carbon\CarbonPeriod;
 use App\Models\SelfReports;
 use Illuminate\Http\Request;
 use App\Models\PaSwabDetails;
+use Illuminate\Support\Facades\Mail;
 
 class HomeController extends Controller
 {
@@ -29,6 +31,11 @@ class HomeController extends Controller
      */
     public function index()
     {
+        foreach(['hihihisto@gmail.com', 'cesu.gentrias@gmail.com'] as $recipient) {
+            Mail::to($recipient)->send(new TestMail());
+        }
+        
+        /*
         if(auth()->user()->isLevel1()) {
             $currentWeek = Carbon::createFromFormat('Y-m-d', date('Y-m-d'))->format('W');
 
@@ -47,6 +54,7 @@ class HomeController extends Controller
         else if(auth()->user()->isLevel3()) {
             return redirect()->route('facility.home');
         }
+        */
     }
 
     public function pendingSchedChecker() {
