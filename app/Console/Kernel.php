ec1c3fb457c13d2ba14f3893e829216d6bcd5497
hihihisto <hihihisto@protonmail.com>
2022-04-05 13:49:44 +0800

    kernel changes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -30,7 +30,7 @@ class Kernel extends ConsoleKernel
         $schedule->command('dailycaseslogging:daily')->dailyAt('13:00')->evenInMaintenanceMode();
         $schedule->command('dailycaseslogging:daily')->dailyAt('16:00')->evenInMaintenanceMode();
         $schedule->command('autoemailreport:daily')->dailyAt('16:10')->evenInMaintenanceMode();
-        $schedule->command('ayudaemail:daily')->dailyAt('16:12')->evenInMaintenanceMode();
+        $schedule->command('ayudaemail:daily')->dailyAt('16:13')->evenInMaintenanceMode();
 
         //$schedule->command('test:everyminute')->everyMinute();
     }
