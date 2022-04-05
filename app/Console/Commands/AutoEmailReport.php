11cc360630b45e015a9ebd097713ddf964c6e8e7
hihihisto <hihihisto@protonmail.com>
2022-04-05 16:53:05 +0800

    added covid db

--- app/Console/Commands/AutoEmailReport.php
+++ app/Console/Commands/AutoEmailReport.php
@@ -232,7 +232,7 @@ class AutoEmailReport extends Command
         $writer = new Xlsx($spreadsheet);
         $writer->save(public_path('GEN.TRIAS-DILG-CHO-REPORT-'.date('F-d-Y').'.xlsx'));
 
-        foreach(['hihihisto@gmail.com', 'cesu.gentrias@gmail.com', 'jango_m14@yahoo.com', 'glorybemendez06@gmail.com'] as $recipient) {
+        foreach(['hihihisto@gmail.com', 'cesu.gentrias@gmail.com', 'jango_m14@yahoo.com', 'ronald888mojica@gmail.com', 'glorybemendez06@gmail.com'] as $recipient) {
             Mail::to($recipient)->send(new CovidReportWord());
         }
 
