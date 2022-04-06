cb53cc22cec1fd667255a6365c4ad3b052106b65
hihihisto <hihihisto@protonmail.com>
2022-04-06 08:57:40 +0800

    added note on email

--- resources/views/email/dilgreport.blade.php
+++ resources/views/email/dilgreport.blade.php
@@ -4,4 +4,11 @@
     <p>As of {{date('F d, Y')}}</p>
     <p>-</p>
     <p>Note: Computer Generated file, Do Not Reply. Made possible by Christian James Historillo.</p>
+    <p></p>
+    <p>Automated Email noted and approved by:</p>
+    <p><b>LUIS P. BROAS,RN,RPh,MAN</b></p>
+    <p><i>Nurse II/CESU Head</i></p>
+    <p><i>City Health Office - General Trias, Cavite</i></p>
+    <p><b>Mobile:</b> +63 917 561 1254</p>
+    <p><b>Email:</b> <a href="mailto: cesu.gentrias@gmail.com">cesu.gentrias@gmail.com</a></p>
 </div>
