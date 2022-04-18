5f7970968cfb541a5f2b72e362925988f5cbce74
hihihisto <hihihisto@protonmail.com>
2022-04-18 08:54:48 +0800

    typo fix

--- resources/views/email/ayudalist_nocase.blade.php
+++ resources/views/email/ayudalist_nocase.blade.php
@@ -1,6 +1,6 @@
 <div>
     <p>Good Day!</p>
-    <p>There are no new documented confirmed cases for today ({{date('F d, Y')}})</p>
+    <p>There are no new encoded confirmed cases for today ({{date('F d, Y')}})</p>
     <p>Therefore, no Ayuda List will be submitted.</p>
     <p>-</p>
     <p>Note: Computer Generated file, Do Not Reply. Made possible by Christian James Historillo.</p>
