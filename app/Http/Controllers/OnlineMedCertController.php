71fa33c223fb2ad2aa3adb20d0812f3e530466bf
hihihisto <hihihisto@protonmail.com>
2022-06-07 14:47:54 +0800

    added outcome conditions

--- app/Http/Controllers/OnlineMedCertController.php
+++ app/Http/Controllers/OnlineMedCertController.php
@@ -41,52 +41,59 @@ class OnlineMedCertController extends Controller
         }
 
         if($b) {
-            $number = date('j');
+            if($b->outcomeCondition == 'Recovered') {
+                $number = date('j');
 
-            $ends = array('th','st','nd','rd','th','th','th','th','th','th');
-            if (($number %100) >= 11 && ($number%100) <= 13) {
-                $abbreviation = $number. 'th';
+                $ends = array('th','st','nd','rd','th','th','th','th','th','th');
+                if (($number %100) >= 11 && ($number%100) <= 13) {
+                    $abbreviation = $number. 'th';
+                }
+                else {
+                    $abbreviation = $number. $ends[$number % 10];
+                }
+
+                if($b->pType == 'CLOSE CONTACT') {
+                    $pui = true;
+                }
+                else {
+                    $pui = false;
+                }
+
+                if(!is_null($b->SAS)) {
+                    $pum = true;
+                }
+                else {
+                    $pum = false;
+                }
+
+                if(!is_null($b->testDateCollected2)) {
+                    $qds = $b->testDateCollected2;
+                }
+                else {
+                    $qds = $b->testDateCollected1;
+                }
+
+                if($b->records->ifFullyVaccinated()) {
+                    $qde = Carbon::parse($qds)->addDays(10)->format('Y-m-d');
+                }
+                else {
+                    $qde = Carbon::parse($qds)->addDays(14)->format('Y-m-d');
+                }
+
+                return view('medcert', [
+                    'data' => $b,
+                    'pui' => $pui,
+                    'pum' => $pum,
+                    'cardinal' => $abbreviation,
+                    'qds' => $qds,
+                    'qde' => $qde,
+                ]);
             }
             else {
-                $abbreviation = $number. $ends[$number % 10];
+                return redirect()->back()
+                ->with('msg', 'Unable to generate. Patient is not yet recovered.')
+                ->with('msgtype', 'danger');
             }
-
-            if($b->pType == 'CLOSE CONTACT') {
-                $pui = true;
-            }
-            else {
-                $pui = false;
-            }
-
-            if(!is_null($b->SAS)) {
-                $pum = true;
-            }
-            else {
-                $pum = false;
-            }
-
-            if(!is_null($b->testDateCollected2)) {
-                $qds = $b->testDateCollected2;
-            }
-            else {
-                $qds = $b->testDateCollected1;
-            }
-
-            if($b->records->ifFullyVaccinated()) {
-                $qde = Carbon::parse($qds)->addDays(10)->format('Y-m-d');
-            }
-            else {
-                $qde = Carbon::parse($qds)->addDays(14)->format('Y-m-d');
-            }
-
-            return view('medcert', [
-                'data' => $b,
-                'pui' => $pui,
-                'pum' => $pum,
-                'cardinal' => $abbreviation,
-                'qds' => $qds,
-                'qde' => $qde,
-            ]);
         }
         else {
             return redirect()->back()
