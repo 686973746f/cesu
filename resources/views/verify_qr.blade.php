4a7febe82d7dfbac9ac93878f5e16cff282fd547
hihihisto <hihihisto@protonmail.com>
2022-10-19 13:03:12 +0800

    veriify fix ui

--- resources/views/verify_qr.blade.php
+++ resources/views/verify_qr.blade.php
@@ -6,17 +6,31 @@
         <div class="card-body">
             <div class="text-center">
                 <img src="{{asset('assets/images/cesu_icon.png')}}" class="mb-3" style="width: 8rem;">
-                <h4>CESU Gen. Trias Swab Test Result Verification</h4>
+                <h4><b>CESU Gen. Trias Swab Test Result Verification</b></h4>
                 <hr>
                 <span>Beware of fake verification sites. The legitimate site should have this domain name https://cesugentri.com/verify/</span>
                 <hr>
                 @if($c)
+                @php
+                if($c->getLatestTestResult() == 'POSITIVE') {
+                    $txtc = 'text-danger';
+                }
+                else if($c->getLatestTestResult() == 'NEGATIVE') {
+                    $txtc = 'text-success';
+                }
+                else if($c->getLatestTestResult() == 'PENDING') {
+                    $txtc = 'text-warning';
+                }
+                else {
+                    $txtc = '';
+                }
+                @endphp
                 <div class="table-responsive">
                     <table class="table table-bordered">
                         <tbody>
                             <tr class="">
                                 <td>Test ID</td>
-                                <td>{{$c->id}}</td>
+                                <td>#{{$c->id}}</td>
                             </tr>
                             <tr class="">
                                 <td>Name</td>
@@ -28,15 +42,15 @@
                             </tr>
                             <tr class="">
                                 <td>Specimen Collection</td>
-                                <td>{{$c->getLatestTestDate()}}</td>
+                                <td>{{date('d-M-Y', strtotime($c->getLatestTestDate()))}}</td>
                             </tr>
                             <tr class="">
                                 <td>Result Released</td>
-                                <td>{{(!is_null($c->getLatestTestDateReleased())) ? $c->getLatestTestDateReleased() : 'N/A'}}</td>
+                                <td>{{(!is_null($c->getLatestTestDateReleased())) ? date('d-M-Y', strtotime($c->getLatestTestDateReleased())) : 'N/A'}}</td>
                             </tr>
                             <tr class="font-weight-bold">
                                 <td>Result</td>
-                                <td>{{$c->getLatestTestResult()}}</td>
+                                <td class="{{$txtc}}">{{$c->getLatestTestResult()}}</td>
                             </tr>
                         </tbody>
                     </table>
