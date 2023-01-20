f14a429fd9bc6374e9a47f61b362cf6d1406a6e3
hihihisto <hihihisto@protonmail.com>
2023-01-20 08:57:31 +0800

    encoder stats

--- app/Mail/SendEncoderStatus.php
+++ app/Mail/SendEncoderStatus.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use App\Models\Forms;
 use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
+use App\Models\AbtcBakunaRecords;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
