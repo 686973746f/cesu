bdbf6219fe6a4dbc1fc11a47048b05d436645852
hihihisto <hihihisto@protonmail.com>
2023-01-26 16:07:10 +0800

    ui changes

--- resources/views/abtc/schedule_index.blade.php
+++ resources/views/abtc/schedule_index.blade.php
@@ -1,6 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
+<style>
+    #ntable td {
+        vertical-align: middle;
+    }
+
+    #fftable td {
+        vertical-align: middle;
+    }
+</style>
 <div class="container">
     <form action="" method="GET">
         <div class="input-group mb-3">
@@ -18,36 +27,38 @@
             </div>
         </div>
         <div class="card-body">
-            <table class="table table-bordered table-striped" id="ntable">
-                <thead class="thead-light text-center">
-                    <tr>
-                        <th>Registration #</th>
-                        <th>Name</th>
-                        <th>Age/Gender</th>
-                        <th>Brgy</th>
-                        <th>Animal</th>
-                        <th>Date Bitten</th>
-                        <th>Category</th>
-                        <th>Body Part</th>
-                        <th>Date Registered</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($new as $n)
-                    <tr>
-                        <td class="text-center"><a href="{{route('abtc_encode_edit', $n->id)}}">{{$n->case_id}}</a></td>
-                        <td>{{$n->patient->getName()}}</td>
-                        <td class="text-center">{{$n->patient->getAge()}} / {{$n->patient->sg()}}</td>
-                        <td class="text-center">{{$n->patient->address_brgy_text}}</td>
-                        <td class="text-center">{{$n->animal_type}}</td>
-                        <td class="text-center">{{date('m/d/Y (D)', strtotime($n->bite_date))}}</td>
-                        <td class="text-center">{{$n->category_level}}</td>
-                        <td class="text-center">{{(!is_null($n->body_site)) ? mb_strtoupper($n->body_site) : 'N/A'}}</td>
-                        <td class="text-center"><small>{{date('m/d/Y H:i:s', strtotime($n->created_at))}}</small></td>
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
+            <div class="table-responsive">
+                <table class="table table-bordered table-striped" id="ntable">
+                    <thead class="thead-light text-center">
+                        <tr>
+                            <th>Registration #</th>
+                            <th>Name</th>
+                            <th>Age/Gender</th>
+                            <th>Brgy</th>
+                            <th>Animal</th>
+                            <th>Date Bitten</th>
+                            <th>Category</th>
+                            <th>Body Part</th>
+                            <th>Date Registered</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($new as $n)
+                        <tr>
+                            <td class="text-center"><a href="{{route('abtc_encode_edit', $n->id)}}">{{$n->case_id}}</a></td>
+                            <td>{{$n->patient->getName()}}</td>
+                            <td class="text-center">{{$n->patient->getAge()}} / {{$n->patient->sg()}}</td>
+                            <td class="text-center"><small>{{$n->patient->address_brgy_text}}</small></td>
+                            <td class="text-center">{{$n->animal_type}}</td>
+                            <td class="text-center">{{date('m/d/Y (D)', strtotime($n->bite_date))}}</td>
+                            <td class="text-center">{{$n->category_level}}</td>
+                            <td class="text-center">{{(!is_null($n->body_site)) ? mb_strtoupper($n->body_site) : 'N/A'}}</td>
+                            <td class="text-center"><small>{{date('m/d/Y H:i:s', strtotime($n->created_at))}}</small></td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
     <div class="card">
@@ -58,38 +69,40 @@
             </div>
         </div>
         <div class="card-body">
-            <table class="table table-bordered table-striped" id="fftable">
-                <thead class="thead-light text-center">
-                    <tr>
-                        <th>Registration #</th>
-                        <th>Name</th>
-                        <th>Age/Gender</th>
-                        <th>Brgy</th>
-                        <th>Animal</th>
-                        <th>Date Bitten</th>
-                        <th>Category</th>
-                        <th>Body Part</th>
-                        <th>Day</th>
-                        <th>Date Registered</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($ff as $n)
-                    <tr>
-                        <td class="text-center"><a href="{{route('abtc_encode_edit', $n->id)}}">{{$n->case_id}}</a></td>
-                        <td>{{$n->patient->getName()}}</td>
-                        <td class="text-center">{{$n->patient->getAge()}} / {{$n->patient->sg()}}</td>
-                        <td class="text-center">{{$n->patient->address_brgy_text}}</td>
-                        <td class="text-center">{{$n->animal_type}}</td>
-                        <td class="text-center">{{date('m/d/Y (D)', strtotime($n->bite_date))}}</td>
-                        <td class="text-center">{{$n->category_level}}</td>
-                        <td class="text-center">{{(!is_null($n->body_site)) ? mb_strtoupper($n->body_site) : 'N/A'}}</td>
-                        <td class="text-center">{{$n->getlatestday()}}</td>
-                        <td class="text-center"><small>{{date('m/d/Y H:i:s', strtotime($n->created_at))}}</small></td>
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
+            <div class="table-responsive">
+                <table class="table table-bordered table-striped" id="fftable">
+                    <thead class="thead-light text-center">
+                        <tr>
+                            <th>Registration #</th>
+                            <th>Name</th>
+                            <th>Age/Gender</th>
+                            <th>Brgy</th>
+                            <th>Animal</th>
+                            <th>Date Bitten</th>
+                            <th>Category</th>
+                            <th>Body Part</th>
+                            <th>Day</th>
+                            <th>Date Registered</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($ff as $n)
+                        <tr>
+                            <td class="text-center"><a href="{{route('abtc_encode_edit', $n->id)}}">{{$n->case_id}}</a></td>
+                            <td>{{$n->patient->getName()}}</td>
+                            <td class="text-center">{{$n->patient->getAge()}} / {{$n->patient->sg()}}</td>
+                            <td class="text-center"><small>{{$n->patient->address_brgy_text}}</small></td>
+                            <td class="text-center">{{$n->animal_type}}</td>
+                            <td class="text-center">{{date('m/d/Y (D)', strtotime($n->bite_date))}}</td>
+                            <td class="text-center">{{$n->category_level}}</td>
+                            <td class="text-center">{{(!is_null($n->body_site)) ? mb_strtoupper($n->body_site) : 'N/A'}}</td>
+                            <td class="text-center">{{$n->getlatestday()}}</td>
+                            <td class="text-center"><small>{{date('m/d/Y H:i:s', strtotime($n->created_at))}}</small></td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 </div>
