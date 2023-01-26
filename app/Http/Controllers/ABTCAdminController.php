84c10367a4fcae8ca5fcc92c34431233f1dd9e5c
hihihisto <hihihisto@protonmail.com>
2023-01-26 15:19:15 +0800

    gupdated rev4

--- app/Http/Controllers/ABTCAdminController.php
+++ app/Http/Controllers/ABTCAdminController.php
@@ -67,7 +67,7 @@ class ABTCAdminController extends Controller
     }
 
     public function gupdate() {
-        $l = AbtcBakunaRecords::where('vaccination_site_id', 1)
+        $l = AbtcBakunaRecords::where('vaccination_site_id', 2)
         ->whereYear('case_date', '2023')
         ->orderBy('created_at', 'ASC')
         ->get();
