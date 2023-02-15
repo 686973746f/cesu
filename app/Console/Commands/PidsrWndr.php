99b7aab367716809add281a75ce94b3dfae98f84
hihihisto <hihihisto@protonmail.com>
2023-02-15 13:56:51 +0800

    import update

--- app/Console/Commands/PidsrWndr.php
+++ app/Console/Commands/PidsrWndr.php
@@ -11,6 +11,7 @@ use App\Models\Nnt;
 use App\Models\Psp;
 use App\Models\Ames;
 use App\Models\Diph;
+use App\Models\Hfmd;
 use App\Models\Pert;
 use App\Models\Chikv;
 use App\Models\Dengue;
@@ -31,9 +32,9 @@ use PhpOffice\PhpWord\PhpWord;
 use Illuminate\Console\Command;
 use PhpOffice\PhpWord\Settings;
 use PhpOffice\PhpWord\IOFactory;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Mail;
 use PhpOffice\PhpWord\TemplateProcessor;
-use Illuminate\Support\Facades\File;
 
 class PidsrWndr extends Command
 {
@@ -93,191 +94,371 @@ class PidsrWndr extends Command
         $afp = Afp::where('Province', 'CAVITE')
         ->where('Muncity', 'GENERAL TRIAS')
         ->where('Year', date('Y'))
-        ->where('MorbidityMonth', date('n'))
-        ->where('MorbidityWeek', date('W'))
+        ->where('MorbidityMonth', date('n', strtotime('-1 Week')))
+        ->where('MorbidityWeek', date('W', strtotime('-1 Week')))
         ->count();
+
+        $afp += Afp::where('Province', 'CAVITE')
+        ->where('Muncity', 'GENERAL TRIAS')
+        ->where('MorbidityMonth', '<=', date('n', strtotime('-2 Weeks')))
+        ->where('MorbidityWeek', '<=', date('W', strtotime('-2 Weeks')))
+        ->whereWeek('created_at', '>=', date('W', strtotime('-1 Week')))
+        ->count();
+
+        $aefi = 0;
 
         $ant = Anthrax::where('Province', 'CAVITE')
         ->where('Muncity', 'GENERAL TRIAS')
         ->where('Year', date('Y'))
-        ->where('MorbidityMonth', date('n'))
-        ->where('MorbidityWeek', date('W'))
+        ->where('MorbidityMonth', date('n', strtotime('-1 Week')))
+        ->where('MorbidityWeek', date('W', strtotime('-1 Week')))
         ->count();
 
-        $inf = Influenza::where('Province', 'CAVITE')
+        $ant += Anthrax::where('Province', 'CAVITE')
         ->where('Muncity', 'GENERAL TRIAS')
-        ->where('Year', date('Y'))
-        ->where('MorbidityMonth', date('n'))
-        ->where('MorbidityWeek', date('W'))
+        ->where('MorbidityMonth', '<=', date('n', strtotime('-2 Weeks')))
+        ->where('MorbidityWeek', '<=', date('W', strtotime('-2 Weeks')))
+        ->whereWeek('created_at', '>=', date('W', strtotime('-1 Week')))
         ->count();
+
+        $hai = 0; 
 
         $mea = Measles::where('Province', 'CAVITE')
         ->where('Muncity', 'GENERAL TRIAS')
         ->where('Year', date('Y'))
-        ->where('MorbidityMonth', date('n'))
-        ->where('MorbidityWeek', date('W'))
+        ->where('MorbidityMonth', date('n', strtotime('-1 Week')))
+        ->where('MorbidityWeek', date('W', strtotime('-1 Week')))
+        ->count();
+
+        $mea += Measles::where('Province', 'CAVITE')
+        ->where('Muncity', 'GENERAL TRIAS')
+        ->where('MorbidityMonth', '<=', date('n', strtotime('-2 Weeks')))
+        ->where('MorbidityWeek', '<=', date('W', strtotime('-2 Weeks')))
+        ->whereWeek('created_at', '>=', date('W', strtotime('-1 Week')))
         ->count();
 
         $mgc = Meningo::where('Province', 'CAVITE')
         ->where('Muncity', 'GENERAL TRIAS')
         ->where('Year', date('Y'))
-        ->where('MorbidityMonth', date('n'))
-        ->where('MorbidityWeek', date('W'))
+        ->where('MorbidityMonth', date('n', strtotime('-1 Week')))
+        ->where('MorbidityWeek', date('W', strtotime('-1 Week')))
+        ->count();
+
+        $mgc += Meningo::where('Province', 'CAVITE')
+        ->where('Muncity', 'GENERAL TRIAS')
+        ->where('MorbidityMonth', '<=', date('n', strtotime('-2 Weeks')))
+        ->where('MorbidityWeek', '<=', date('W', strtotime('-2 Weeks')))
+        ->whereWeek('created_at', '>=', date('W', strtotime('-1 Week')))
         ->count();
 
         $nt = Nt::where('Province', 'CAVITE')
         ->where('Muncity', 'GENERAL TRIAS')
         ->where('Year', date('Y'))
-        ->where('MorbidityMonth', date('n'))
-        ->where('MorbidityWeek', date('W'))
+        ->where('MorbidityMonth', date('n', strtotime('-1 Week')))
+        ->where('MorbidityWeek', date('W', strtotime('-1 Week')))
+        ->count();
+
+        $nt += Nt::where('Province', 'CAVITE')
+        ->where('Muncity', 'GENERAL TRIAS')
+        ->where('MorbidityMonth', '<=', date('n', strtotime('-2 Weeks')))
+        ->where('MorbidityWeek', '<=', date('W', strtotime('-2 Weeks')))
+        ->whereWeek('created_at', '>=', date('W', strtotime('-1 Week')))
         ->count();
 
         $psp = Psp::where('Province', 'CAVITE')
         ->where('Muncity', 'GENERAL TRIAS')
         ->where('Year', date('Y'))
-        ->where('MorbidityMonth', date('n'))
-        ->where('MorbidityWeek', date('W'))
+        ->where('MorbidityMonth', date('n', strtotime('-1 Week')))
+        ->where('MorbidityWeek', date('W', strtotime('-1 Week')))
+        ->count();
+
+        $psp += Psp::where('Province', 'CAVITE')
+        ->where('Muncity', 'GENERAL TRIAS')
+        ->where('MorbidityMonth', '<=', date('n', strtotime('-2 Weeks')))
+        ->where('MorbidityWeek', '<=', date('W', strtotime('-2 Weeks')))
+        ->whereWeek('created_at', '>=', date('W', strtotime('-1 Week')))
         ->count();
 
         $rab = Rabies::where('Province', 'CAVITE')
         ->where('Muncity', 'GENERAL TRIAS')
         ->where('Year', date('Y'))
-        ->where('MorbidityMonth', date('n'))
-        ->where('MorbidityWeek', date('W'))
+        ->where('MorbidityMonth', date('n', strtotime('-1 Week')))
+        ->where('MorbidityWeek', date('W', strtotime('-1 Week')))
         ->count();
 
-        $sar = Rabies::where('Province', 'CAVITE')
+        $rab += Rabies::where('Province', 'CAVITE')
         ->where('Muncity', 'GENERAL TRIAS')
-        ->where('Year', date('Y'))
-        ->where('MorbidityMonth', date('n'))
-        ->where('MorbidityWeek', date('W'))
+        ->where('MorbidityMonth', '<=', date('n', strtotime('-2 Weeks')))
+        ->where('MorbidityWeek', '<=', date('W', strtotime('-2 Weeks')))
+        ->whereWeek('created_at', '>=', date('W', strtotime('-1 Week')))
         ->count();
+
+        $sar = 0;
 
         $abd = Abd::where('Province', 'CAVITE')
         ->where('Muncity', 'GENERAL TRIAS')
         ->where('Year', date('Y'))
-        ->where('MorbidityMonth', date('n'))
-        ->where('MorbidityWeek', date('W'))
+        ->where('MorbidityMonth', date('n', strtotime('-1 Week')))
+        ->where('MorbidityWeek', date('W', strtotime('-1 Week')))
+        ->count();
+
+        $abd += Abd::where('Province', 'CAVITE')
+        ->where('Muncity', 'GENERAL TRIAS')
+        ->where('MorbidityMonth', '<=', date('n', strtotime('-2 Weeks')))
+        ->where('MorbidityWeek', '<=', date('W', strtotime('-2 Weeks')))
+        ->whereWeek('created_at', '>=', date('W', strtotime('-1 Week')))
         ->count();
 
         $aes = Aes::where('Province', 'CAVITE')
         ->where('Muncity', 'GENERAL TRIAS')
         ->where('Year', date('Y'))
-        ->where('MorbidityMonth', date('n'))
-        ->where('MorbidityWeek', date('W'))
+        ->where('MorbidityMonth', date('n', strtotime('-1 Week')))
+        ->where('MorbidityWeek', date('W', strtotime('-1 Week')))
+        ->count();
+
+        $aes += Aes::where('Province', 'CAVITE')
+        ->where('Muncity', 'GENERAL TRIAS')
+        ->where('MorbidityMonth', '<=', date('n', strtotime('-2 Weeks')))
+        ->where('MorbidityWeek', '<=', date('W', strtotime('-2 Weeks')))
+        ->whereWeek('created_at', '>=', date('W', strtotime('-1 Week')))
         ->count();
 
         $ahf = Ahf::where('Province', 'CAVITE')
         ->where('Muncity', 'GENERAL TRIAS')
         ->where('Year', date('Y'))
-        ->where('MorbidityMonth', date('n'))
-        ->where('MorbidityWeek', date('W'))
+        ->where('MorbidityMonth', date('n', strtotime('-1 Week')))
+        ->where('MorbidityWeek', date('W', strtotime('-1 Week')))
+        ->count();
+
+        $ahf += Ahf::where('Province', 'CAVITE')
+        ->where('Muncity', 'GENERAL TRIAS')
+        ->where('MorbidityMonth', '<=', date('n', strtotime('-2 Weeks')))
+        ->where('MorbidityWeek', '<=', date('W', strtotime('-2 Weeks')))
+        ->whereWeek('created_at', '>=', date('W', strtotime('-1 Week')))
         ->count();
 
         $hep = Hepatitis::where('Province', 'CAVITE')
         ->where('Muncity', 'GENERAL TRIAS')
         ->where('Year', date('Y'))
-        ->where('MorbidityMonth', date('n'))
-        ->where('MorbidityWeek', date('W'))
+        ->where('MorbidityMonth', date('n', strtotime('-1 Week')))
+        ->where('MorbidityWeek', date('W', strtotime('-1 Week')))
+        ->count();
+
+        $hep += Hepatitis::where('Province', 'CAVITE')
+        ->where('Muncity', 'GENERAL TRIAS')
+        ->where('MorbidityMonth', '<=', date('n', strtotime('-2 Weeks')))
+        ->where('MorbidityWeek', '<=', date('W', strtotime('-2 Weeks')))
+        ->whereWeek('created_at', '>=', date('W', strtotime('-1 Week')))
         ->count();
 
         $ame = Ames::where('Province', 'CAVITE')
         ->where('Muncity', 'GENERAL TRIAS')
         ->where('Year', date('Y'))
-        ->where('MorbidityMonth', date('n'))
-        ->where('MorbidityWeek', date('W'))
+        ->where('MorbidityMonth', date('n', strtotime('-1 Week')))
+        ->where('MorbidityWeek', date('W', strtotime('-1 Week')))
+        ->count();
+
+        $ame += Ames::where('Province', 'CAVITE')
+        ->where('Muncity', 'GENERAL TRIAS')
+        ->where('MorbidityMonth', '<=', date('n', strtotime('-2 Weeks')))
+        ->where('MorbidityWeek', '<=', date('W', strtotime('-2 Weeks')))
+        ->whereWeek('created_at', '>=', date('W', strtotime('-1 Week')))
         ->count();
 
         $mgt = Meningitis::where('Province', 'CAVITE')
         ->where('Muncity', 'GENERAL TRIAS')
         ->where('Year', date('Y'))
-        ->where('MorbidityMonth', date('n'))
-        ->where('MorbidityWeek', date('W'))
+        ->where('MorbidityMonth', date('n', strtotime('-1 Week')))
+        ->where('MorbidityWeek', date('W', strtotime('-1 Week')))
+        ->count();
+
+        $mgt += Meningitis::where('Province', 'CAVITE')
+        ->where('Muncity', 'GENERAL TRIAS')
+        ->where('MorbidityMonth', '<=', date('n', strtotime('-2 Weeks')))
+        ->where('MorbidityWeek', '<=', date('W', strtotime('-2 Weeks')))
+        ->whereWeek('created_at', '>=', date('W', strtotime('-1 Week')))
         ->count();
 
         $chi = Chikv::where('Province', 'CAVITE')
         ->where('Muncity', 'GENERAL TRIAS')
         ->where('Year', date('Y'))
-        ->where('MorbidityMonth', date('n'))
-        ->where('MorbidityWeek', date('W'))
+        ->where('MorbidityMonth', date('n', strtotime('-1 Week')))
+        ->where('MorbidityWeek', date('W', strtotime('-1 Week')))
+        ->count();
+
+        $chi += Chikv::where('Province', 'CAVITE')
+        ->where('Muncity', 'GENERAL TRIAS')
+        ->where('MorbidityMonth', '<=', date('n', strtotime('-2 Weeks')))
+        ->where('MorbidityWeek', '<=', date('W', strtotime('-2 Weeks')))
+        ->whereWeek('created_at', '>=', date('W', strtotime('-1 Week')))
         ->count();
 
         $cho = Cholera::where('Province', 'CAVITE')
         ->where('Muncity', 'GENERAL TRIAS')
         ->where('Year', date('Y'))
-        ->where('MorbidityMonth', date('n'))
-        ->where('MorbidityWeek', date('W'))
+        ->where('MorbidityMonth', date('n', strtotime('-1 Week')))
+        ->where('MorbidityWeek', date('W', strtotime('-1 Week')))
+        ->count();
+
+        $cho += Cholera::where('Province', 'CAVITE')
+        ->where('Muncity', 'GENERAL TRIAS')
+        ->where('MorbidityMonth', '<=', date('n', strtotime('-2 Weeks')))
+        ->where('MorbidityWeek', '<=', date('W', strtotime('-2 Weeks')))
+        ->whereWeek('created_at', '>=', date('W', strtotime('-1 Week')))
         ->count();
 
         $den = Dengue::where('Province', 'CAVITE')
         ->where('Muncity', 'GENERAL TRIAS')
         ->where('Year', date('Y'))
-        ->where('MorbidityMonth', date('n'))
-        ->where('MorbidityWeek', date('W'))
+        ->where('MorbidityMonth', date('n', strtotime('-1 Week')))
+        ->where('MorbidityWeek', date('W', strtotime('-1 Week')))
+        ->count();
+
+        $den += Dengue::where('Province', 'CAVITE')
+        ->where('Muncity', 'GENERAL TRIAS')
+        ->where('MorbidityMonth', '<=', date('n', strtotime('-2 Weeks')))
+        ->where('MorbidityWeek', '<=', date('W', strtotime('-2 Weeks')))
+        ->whereWeek('created_at', '>=', date('W', strtotime('-1 Week')))
         ->count();
 
         $dip = Diph::where('Province', 'CAVITE')
         ->where('Muncity', 'GENERAL TRIAS')
         ->where('Year', date('Y'))
-        ->where('MorbidityMonth', date('n'))
-        ->where('MorbidityWeek', date('W'))
+        ->where('MorbidityMonth', date('n', strtotime('-1 Week')))
+        ->where('MorbidityWeek', date('W', strtotime('-1 Week')))
         ->count();
 
-        $ili = 0;
+        $dip += Diph::where('Province', 'CAVITE')
+        ->where('Muncity', 'GENERAL TRIAS')
+        ->where('MorbidityMonth', '<=', date('n', strtotime('-2 Weeks')))
+        ->where('MorbidityWeek', '<=', date('W', strtotime('-2 Weeks')))
+        ->whereWeek('created_at', '>=', date('W', strtotime('-1 Week')))
+        ->count();
+
+        $ili = Influenza::where('Province', 'CAVITE')
+        ->where('Muncity', 'GENERAL TRIAS')
+        ->where('Year', date('Y'))
+        ->where('MorbidityMonth', date('n', strtotime('-1 Week')))
+        ->where('MorbidityWeek', date('W', strtotime('-1 Week')))
+        ->count();
+
+        $ili += Influenza::where('Province', 'CAVITE')
+        ->where('Muncity', 'GENERAL TRIAS')
+        ->where('MorbidityMonth', '<=', date('n', strtotime('-2 Weeks')))
+        ->where('MorbidityWeek', '<=', date('W', strtotime('-2 Weeks')))
+        ->whereWeek('created_at', '>=', date('W', strtotime('-1 Week')))
+        ->count();
 
         $lep = Leptospirosis::where('Province', 'CAVITE')
         ->where('Muncity', 'GENERAL TRIAS')
         ->where('Year', date('Y'))
-        ->where('MorbidityMonth', date('n'))
-        ->where('MorbidityWeek', date('W'))
+        ->where('MorbidityMonth', date('n', strtotime('-1 Week')))
+        ->where('MorbidityWeek', date('W', strtotime('-1 Week')))
+        ->count();
+
+        $lep += Leptospirosis::where('Province', 'CAVITE')
+        ->where('Muncity', 'GENERAL TRIAS')
+        ->where('MorbidityMonth', '<=', date('n', strtotime('-2 Weeks')))
+        ->where('MorbidityWeek', '<=', date('W', strtotime('-2 Weeks')))
+        ->whereWeek('created_at', '>=', date('W', strtotime('-1 Week')))
         ->count();
 
         $mal = Malaria::where('Province', 'CAVITE')
         ->where('Muncity', 'GENERAL TRIAS')
         ->where('Year', date('Y'))
-        ->where('MorbidityMonth', date('n'))
-        ->where('MorbidityWeek', date('W'))
+        ->where('MorbidityMonth', date('n', strtotime('-1 Week')))
+        ->where('MorbidityWeek', date('W', strtotime('-1 Week')))
+        ->count();
+
+        $mal += Malaria::where('Province', 'CAVITE')
+        ->where('Muncity', 'GENERAL TRIAS')
+        ->where('MorbidityMonth', '<=', date('n', strtotime('-2 Weeks')))
+        ->where('MorbidityWeek', '<=', date('W', strtotime('-2 Weeks')))
+        ->whereWeek('created_at', '>=', date('W', strtotime('-1 Week')))
         ->count();
 
         $nnt = Nnt::where('Province', 'CAVITE')
         ->where('Muncity', 'GENERAL TRIAS')
         ->where('Year', date('Y'))
-        ->where('MorbidityMonth', date('n'))
-        ->where('MorbidityWeek', date('W'))
+        ->where('MorbidityMonth', date('n', strtotime('-1 Week')))
+        ->where('MorbidityWeek', date('W', strtotime('-1 Week')))
+        ->count();
+
+        $nnt += Nnt::where('Province', 'CAVITE')
+        ->where('Muncity', 'GENERAL TRIAS')
+        ->where('MorbidityMonth', '<=', date('n', strtotime('-2 Weeks')))
+        ->where('MorbidityWeek', '<=', date('W', strtotime('-2 Weeks')))
+        ->whereWeek('created_at', '>=', date('W', strtotime('-1 Week')))
         ->count();
 
         $per = Pert::where('Province', 'CAVITE')
         ->where('Muncity', 'GENERAL TRIAS')
         ->where('Year', date('Y'))
-        ->where('MorbidityMonth', date('n'))
-        ->where('MorbidityWeek', date('W'))
+        ->where('MorbidityMonth', date('n', strtotime('-1 Week')))
+        ->where('MorbidityWeek', date('W', strtotime('-1 Week')))
+        ->count();
+
+        $per += Pert::where('Province', 'CAVITE')
+        ->where('Muncity', 'GENERAL TRIAS')
+        ->where('MorbidityMonth', '<=', date('n', strtotime('-2 Weeks')))
+        ->where('MorbidityWeek', '<=', date('W', strtotime('-2 Weeks')))
+        ->whereWeek('created_at', '>=', date('W', strtotime('-1 Week')))
         ->count();
 
         $rtv = Rotavirus::where('Province', 'CAVITE')
         ->where('Muncity', 'GENERAL TRIAS')
         ->where('Year', date('Y'))
-        ->where('MorbidityMonth', date('n'))
-        ->where('MorbidityWeek', date('W'))
+        ->where('MorbidityMonth', date('n', strtotime('-1 Week')))
+        ->where('MorbidityWeek', date('W', strtotime('-1 Week')))
+        ->count();
+
+        $rtv += Rotavirus::where('Province', 'CAVITE')
+        ->where('Muncity', 'GENERAL TRIAS')
+        ->where('MorbidityMonth', '<=', date('n', strtotime('-2 Weeks')))
+        ->where('MorbidityWeek', '<=', date('W', strtotime('-2 Weeks')))
+        ->whereWeek('created_at', '>=', date('W', strtotime('-1 Week')))
         ->count();
 
         $typ = Typhoid::where('Province', 'CAVITE')
         ->where('Muncity', 'GENERAL TRIAS')
         ->where('Year', date('Y'))
-        ->where('MorbidityMonth', date('n'))
-        ->where('MorbidityWeek', date('W'))
+        ->where('MorbidityMonth', date('n', strtotime('-1 Week')))
+        ->where('MorbidityWeek', date('W', strtotime('-1 Week')))
+        ->count();
+
+        $typ += Typhoid::where('Province', 'CAVITE')
+        ->where('Muncity', 'GENERAL TRIAS')
+        ->where('MorbidityMonth', '<=', date('n', strtotime('-2 Weeks')))
+        ->where('MorbidityWeek', '<=', date('W', strtotime('-2 Weeks')))
+        ->whereWeek('created_at', '>=', date('W', strtotime('-1 Week')))
+        ->count();
+
+        $hfm = Hfmd::where('Province', 'CAVITE')
+        ->where('Muncity', 'GENERAL TRIAS')
+        ->where('Year', date('Y'))
+        ->where('MorbidityMonth', date('n', strtotime('-1 Week')))
+        ->where('MorbidityWeek', date('W', strtotime('-1 Week')))
+        ->count();
+
+        $hfm += Hfmd::where('Province', 'CAVITE')
+        ->where('Muncity', 'GENERAL TRIAS')
+        ->where('MorbidityMonth', '<=', date('n', strtotime('-2 Weeks')))
+        ->where('MorbidityWeek', '<=', date('W', strtotime('-2 Weeks')))
+        ->whereWeek('created_at', '>=', date('W', strtotime('-1 Week')))
         ->count();
 
         //Category 1
         $templateProcessor->setValue('afp', $afp);
-        $templateProcessor->setValue('aef', 0);
+        $templateProcessor->setValue('aef', $aefi); //0
         $templateProcessor->setValue('ant', $ant);
-        $templateProcessor->setValue('inf', $inf);
+        $templateProcessor->setValue('inf', $hai); //0
         $templateProcessor->setValue('mea', $mea);
         $templateProcessor->setValue('mgc', $mgc);
         $templateProcessor->setValue('nt', $nt);
         $templateProcessor->setValue('psp', $psp);
         $templateProcessor->setValue('rab', $rab);
-        $templateProcessor->setValue('sar', 0);
+        $templateProcessor->setValue('sar', 0); //0
+        $templateProcessor->setValue('hfm', $hfm);
 
         //Category 2
         $templateProcessor->setValue('abd', $abd);
