4e473f4f8088c5c08fc2ab524cd97a9020116b71
hihihisto <hihihisto@protonmail.com>
2023-05-19 16:48:28 +0800

    schedule fix

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -38,7 +38,7 @@ class Kernel extends ConsoleKernel
         $schedule->command('compositemeasurev2:on15and30')->monthlyOn(15, '16:50');
         $schedule->command('compositemeasurev2:on15and30')->monthlyOn(date('t'), '16:50');
         $schedule->command('pisdrwndr:weekly')->weeklyOn(2, '11:00')->evenInMaintenanceMode();
-        $schedule->command('covidvaccinelinelistimporter:weekly')->weeklyOn(1, '21:00')->evenInMaintenanceMode();
+        $schedule->command('covidvaccinelinelistimporter:weekly')->weeklyOn(1, '22:00')->evenInMaintenanceMode();
         //$schedule->command('queue:work')->everyMinute()->withoutOverlapping();
         
         //$schedule->command('autoemailcompositemeasure:on15and30')->monthlyOn(15, '16:05');
