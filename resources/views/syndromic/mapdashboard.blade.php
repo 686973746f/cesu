d84604872131bd7841129686b874b48de0408085
hihihisto <hihihisto@protonmail.com>
2023-07-11 16:53:56 +0800

    added leaflet css

--- resources/views/syndromic/mapdashboard.blade.php
+++ resources/views/syndromic/mapdashboard.blade.php
@@ -1,30 +1,36 @@
 @extends('layouts.app')
 
 @section('content')
+
+<style>
+    #map { height: 1000px; }
+</style>
+
+
+
 <div class="container-fluid">
     <div class="card">
         <div class="card-header">Map</div>
         <div class="card-body">
-            <div id="map" style="height: 400px;"></div>
+            
             <div class="row">
                 <div class="col-md-6">
 
                 </div>
                 <div class="col-md-6">
-                    
+                    <div id="map"></div>
                 </div>
             </div>
         </div>
     </div>
 </div>
-<script>
-    var map = L.map('map').setView([14.3895, 120.8777], 13);
+<script type="text/javascript">
+    var map = L.map('map').setView([14.3895, 120.8777], 14);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
+    attribution: '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+    
     }).addTo(map);
-
-    // Add markers, polygons, etc. as needed
 
 </script>
 @endsection
