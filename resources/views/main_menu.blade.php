89f226c3dc91b810ee5bfe1d23655f7108cc5f6f
hihihisto <hihihisto@protonmail.com>
2023-08-08 14:13:59 +0800

    main menu permission fix

--- resources/views/main_menu.blade.php
+++ resources/views/main_menu.blade.php
@@ -5,13 +5,25 @@
     <div class="card">
         <div class="card-header"><b>Main Menu</b></div>
         <div class="card-body">
+            @if(auth()->user()->canAccessCovid())
             <a href="{{route('covid_home')}}" class="btn btn-block btn-primary">COVID-19</a>
+            @endif
+            @if(auth()->user()->canAccessAbtc())
             <a href="{{route('abtc_home')}}" class="btn btn-block btn-primary">Animal Bite (ABTC)</a>
+            @endif
+            @if(auth()->user()->canAccessVaxcert())
             <a href="{{route('vaxcert_home')}}" class="btn btn-block btn-primary">VaxCert Concerns</a>
+            @endif
+            @if(auth()->user()->canAccessSyndromic())
             <a href="{{route('syndromic_home')}}" class="btn btn-block btn-primary">Syndromic (Individual Treatment Records - ITR)</a>
+            @endif
             <hr>
+            @if(auth()->user()->canAccessPidsr())
             <a href="{{route('pidsr.home')}}" class="btn btn-block btn-primary">PIDSR (Integrated)</a>
+            @endif
+            @if(auth()->user()->canAccessFhsis())
             <a href="{{route('fhsis_home')}}" class="btn btn-block btn-primary">eFHSIS (Integrated)</a>
+            @endif
         </div>
     </div>
 </div>
