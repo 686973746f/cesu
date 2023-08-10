4c4d906a4df00fd371c3ba03d24c554b84709bc6
hihihisto <hihihisto@protonmail.com>
2023-08-10 08:49:51 +0800

    CAE update

--- resources/views/email/ctreport.blade.php
+++ resources/views/email/ctreport.blade.php
@@ -5,8 +5,8 @@
     <p>Note: Computer Generated file, Do Not Reply. Made possible by Christian James Historillo.</p>
     <br>
     <p>Automated Email noted and approved by:</p>
-    <p><b>LUIS P. BROAS,RN,RPh,MAN</b></p>
-    <p><i>Nurse II/CESU Head</i></p>
+    <p><b>LUIS P. BROAS, RN, RPh, MAN, CAE</b></p>
+    <p><i>Nurse II - CESU Designated Head</i></p>
     <p><i>City Health Office - General Trias, Cavite</i></p>
     <p><b>Mobile:</b> +63 917 561 1254</p>
     <p><b>Email:</b> <a href="mailto:cesu.gentrias@gmail.com">cesu.gentrias@gmail.com</a></p>
--- resources/views/email/test.blade.php
+++ resources/views/email/test.blade.php
@@ -2,8 +2,8 @@
     <p>Test Mail Queue. ({{$bilat}})</p>
     <br>
     <p>Automated Email noted and approved by:</p>
-    <p><b>LUIS P. BROAS,RN,RPh,MAN</b></p>
-    <p><i>Nurse II/CESU Head</i></p>
+    <p><b>LUIS P. BROAS, RN, RPh, MAN, CAE</b></p>
+    <p><i>Nurse II - CESU Designated Head</i></p>
     <p><i>City Health Office - General Trias, Cavite</i></p>
     <p><b>Mobile:</b> +63 917 561 1254</p>
     <p><b>Email:</b> <a href="mailto:cesu.gentrias@gmail.com">cesu.gentrias@gmail.com</a></p>
