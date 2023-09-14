2ab8be47a80fe17434c1558345086521a395da67
hihihisto <hihihisto@protonmail.com>
2023-09-14 15:17:15 +0800

    added fake chart

--- resources/views/main_menu.blade.php
+++ resources/views/main_menu.blade.php
@@ -31,4 +31,59 @@
         </div>
     </div>
 </div>
+
+<div class="container">
+    <canvas id="myChart"></canvas>
+</div>
+
+<script>
+    // Fetch your data here and format it as needed for the chart
+    var data = {
+        labels: ["Label 1", "Label 2", "Label 3"],
+        datasets: [{
+            label: "Chart Title",
+            data: [10, 20, 30], // Replace with your data
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)'
+            ],
+            borderWidth: 1
+        }]
+    };
+
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: data,
+        options: {
+            responsive: true,
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            },
+            plugins: {
+                datalabels: { // Enable the datalabels plugin
+                    color: 'black', // Label text color
+                    anchor: 'end', // Label text anchor position (e.g., 'end', 'start', 'center')
+                    align: 'top', // Label text alignment (e.g., 'top', 'bottom', 'center')
+                    font: {
+                        weight: 'bold' // Label text font weight
+                    },
+                    formatter: function(value) {
+                        // Customize label format as needed
+                        return value;
+                    }
+                }
+            }
+        }
+    });
+</script>
+
 @endsection
