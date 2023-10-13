4c473e161658517ca26a7f7ae2173731c2d2bf17
hihihisto <hihihisto@protonmail.com>
2023-10-13 16:58:52 +0800

    filter fix

--- resources/views/email/syndromiclistsender.blade.php
+++ resources/views/email/syndromiclistsender.blade.php
@@ -3,6 +3,7 @@
     <p></p>
     <ul>
         @foreach($list as $d)
+        @if($d->getListOfSuspDiseases() != 'N/A')
         <li>
             <div>{{$d->syndromic_patient->getName()}}</div>
             <div>{{$d->syndromic_patient->getContactNumber()}}</div>
@@ -10,6 +11,7 @@
             <div>Symptoms: {{$d->listSymptoms()}}</div>
             <div>List of Suspected Disease/s: {{$d->getListOfSuspDiseases()}}</div>
         </li>
+        @endif
         @endforeach
     </ul>
 </div>
