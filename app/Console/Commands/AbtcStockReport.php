81e98291e0b9a79cdb6001e22cdec52238b8aed5
Histo <hihihisto@protonmail.com>
2023-11-15 12:59:01 +0800

    route fix 2

--- app/Console/Commands/AbtcStockReport.php
+++ app/Console/Commands/AbtcStockReport.php
@@ -13,7 +13,7 @@ use App\Models\AbtcVaccinationSite;
 use App\Models\AbtcVaccineBrand;
 use Illuminate\Support\Facades\Mail;
 
-class AbtcStockReportOld extends Command
+class AbtcStockReport extends Command
 {
     /**
      * The name and signature of the console command.
