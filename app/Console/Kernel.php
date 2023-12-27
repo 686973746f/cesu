426c4213cc8da885f5c3ce7e8eeebf1d52c5ef68
Histo <hihihisto@protonmail.com>
2023-12-27 08:05:23 +0800

    vaxcert update daily

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -47,7 +47,7 @@ class Kernel extends ConsoleKernel
         //$schedule->command('compositemeasurev2:on15and30')->monthlyOn(15, '16:50');
         //$schedule->command('compositemeasurev2:on15and30')->monthlyOn(date('t'), '16:50');
         //$schedule->command('pidsrwndr:weekly')->weeklyOn(2, '11:00')->evenInMaintenanceMode();
-        $schedule->command('covidvaccinelinelistimporter:weekly')->weeklyOn(1, '22:00')->evenInMaintenanceMode();
+        $schedule->command('covidvaccinelinelistimporter:weekly')->dailyAt('22:00')->evenInMaintenanceMode();
 
         if(date('w', strtotime(date('Y-m-'.date('t')))) == 6 || date('w', strtotime(date('Y-m-'.date('t')))) == 0) {
             $lastDay = strtotime(date('Y-m-' . date('t')));
