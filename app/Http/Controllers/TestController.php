6ef1ce30b609055476c2d697a625e8ace04f0912
Histo <hihihisto@protonmail.com>
2024-05-06 20:29:37 +0800

    remove test

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,20 +2,19 @@
 
 namespace App\Http\Controllers;
 
-use Carbon\Carbon;
-use App\Models\Brgy;
-use App\Mail\TestMail;
-use App\Models\Measles;
+use App\Imports\DohFacilityImport;
 use App\Imports\EdcsImport;
-use Illuminate\Http\Request;
 use App\Imports\TkcExcelImport;
+use Carbon\Carbon;
+use App\Mail\TestMail;
+use Illuminate\Http\Request;
 use App\Models\AbtcVaccineLogs;
 use App\Models\AbtcVaccineBrand;
 use App\Models\AbtcBakunaRecords;
 use App\Models\AbtcVaccineStocks;
-use App\Imports\DohFacilityImport;
 use Illuminate\Support\Facades\DB;
 use App\Models\AbtcVaccinationSite;
+use App\Models\Measles;
 use Illuminate\Support\Facades\Mail;
 use Maatwebsite\Excel\Facades\Excel;
 
@@ -33,10 +32,7 @@ ALTER TABLE syndromic_records ADD CONSTRAINT `syndromic_records_facility_id_fore
 class TestController extends Controller
 {
     public function index() {
-        $get_list = Brgy::where('city_id', 1)->get();
-
-        return view('test', [
-            'get_list' => $get_list,
-        ]);
+        //Carbon::parse('2024-01-01')->next(Carbon::SATURDAY)->format('m/d/Y')
+        dd(Carbon::parse('2024-01-09')->startOfWeek()->format('m/d/Y'));
     }
 }
--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -5,9 +5,7 @@
         <div class="card">
             <div class="card-header">Test</div>
             <div class="card-body">
-                @foreach($get_list as $d)
-                <a href="{{route('edcs_barangay_quicklogin', ['brgy' => $d->brgyName, 'qlcode' => $d->edcs_quicklogin_code])}}">{{$d->brgyName}}</a>
-                @endforeach
+                
             </div>
         </div>
     </div>
