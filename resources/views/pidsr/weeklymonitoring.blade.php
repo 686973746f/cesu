d8dd01d7d48fd00df6d2d7b1b06315f836420af6
Histo <hihihisto@protonmail.com>
2024-06-26 10:11:07 +0800

    weekly monitoring responsive fix

--- resources/views/pidsr/weeklymonitoring.blade.php
+++ resources/views/pidsr/weeklymonitoring.blade.php
@@ -5,36 +5,38 @@
         <div class="card">
             <div class="card-header"><b>Weekly Submission Checker</b></div>
             <div class="card-body">
-                <table class="table table-striped table-bordered">
-                    <thead class="thead-light text-center">
-                        <tr>
-                            <th>#</th>
-                            <th>Facility 2024</th>
-                            @for($i=1; $i <= $maxweek; $i++)
-                            <th>MW{{$i}}</th>
-                            @endfor
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach($final_array as $ind => $i)
-                        <tr>
-                            <td class="text-center">{{$ind + 1}}</td>
-                            <td><b>{{$i['name']}}</b></td>
-                            @foreach($i['weeks'] as $w)
-                            @php
-                            if($w == '✔') {
-                                $text_color = 'bg-success';
-                            }
-                            else {
-                                $text_color = 'bg-warning';
-                            }
-                            @endphp
-                            <td class="text-center {{$text_color}}">{{$w}}</td>
+                <div class="table-responsive">
+                    <table class="table table-striped table-bordered">
+                        <thead class="thead-light text-center">
+                            <tr>
+                                <th>#</th>
+                                <th>Facility 2024</th>
+                                @for($i=1; $i <= $maxweek; $i++)
+                                <th>MW{{$i}}</th>
+                                @endfor
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($final_array as $ind => $i)
+                            <tr>
+                                <td class="text-center">{{$ind + 1}}</td>
+                                <td><b>{{$i['name']}}</b></td>
+                                @foreach($i['weeks'] as $w)
+                                @php
+                                if($w == '✔') {
+                                    $text_color = 'bg-success';
+                                }
+                                else {
+                                    $text_color = 'bg-warning';
+                                }
+                                @endphp
+                                <td class="text-center {{$text_color}}">{{$w}}</td>
+                                @endforeach
+                            </tr>
                             @endforeach
-                        </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+                        </tbody>
+                    </table>
+                </div>
 
                 <ul>
                     Legend
