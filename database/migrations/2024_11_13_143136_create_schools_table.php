81ade94ce4ca2aa0977709bc633a82338b5bcee0
Histo <hihihisto@protonmail.com>
2024-11-14 10:21:38 +0800

    school db

--- database/migrations/2024_11_13_143136_create_schools_table.php
+++ database/migrations/2024_11_13_143136_create_schools_table.php
@@ -15,6 +15,18 @@ class CreateSchoolsTable extends Migration
     {
         Schema::create('schools', function (Blueprint $table) {
             $table->id();
+            $table->string('enabled', 1)->default('Y');
+            $table->string('name');
+            $table->string('school_type');
+            $table->string('school_id')->nullable();
+            $table->foreignId('address_brgy_code')->constrained('edcs_brgies')->onDelete('cascade');
+            $table->string('contact_number')->nullable();
+            $table->string('contact_number_telephone')->nullable();
+            $table->string('email')->nullable();
+            $table->string('schoolhead_name')->nullable();
+            $table->string('schoolhead_position')->nullable();
+            $table->string('focalperson_name')->nullable();
+            $table->string('longlat')->nullable();
             $table->timestamps();
         });
     }
