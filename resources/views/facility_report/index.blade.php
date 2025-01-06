81f965562dfd95dff34534adfd7d6ce2804d0a3e
Histo <hihihisto@protonmail.com>
2025-01-06 15:17:42 +0800

    fwri route fix

--- resources/views/facility_report/index.blade.php
+++ resources/views/facility_report/index.blade.php
@@ -5,8 +5,6 @@
         <div class="card">
             <div class="card-header"><b>CESU General Trias - Facility Reporting Tool</b></div>
             <div class="card-body">
-                
-                <a href="{{route('fwri_index', $d->sys_code1)}}" class="btn btn-primary btn-lg btn-block">Report Fireworks-Related Injury</a>
                 <a href="{{route('facility_report_injury_index', $d->sys_code1)}}" class="btn btn-primary btn-lg btn-block">Report Vehicular Accident and other Injuries</a>
                 <button type="button" class="btn btn-primary btn-lg btn-block" data-toggle="modal" data-target="#addCase">Encode Case of Reportable Disease</button>
                 <hr>
