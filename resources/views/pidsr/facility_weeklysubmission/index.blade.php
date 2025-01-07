9027a84b8c721c0eacca638d4d3427061a7dc598
Histo <hihihisto@protonmail.com>
2025-01-07 13:15:20 +0800

    edcs modal fix

--- resources/views/pidsr/facility_weeklysubmission/index.blade.php
+++ resources/views/pidsr/facility_weeklysubmission/index.blade.php
@@ -219,25 +219,6 @@
             </div>
         </div>
     </form>
-
-    <div class="modal fade" id="warningModal" tabindex="-1" role="dialog">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title">Modal title</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                </div>
-                <div class="modal-body">
-                    <h4><b>NOTE:</b> Please encode the cases at the EDCS-IS Website first () before proceeding.</h4>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-success btn-block" data-dismiss="modal">Close</button>
-                </div>
-            </div>
-        </div>
-    </div>
     
     <script>
         $('#status').change(function (e) { 
