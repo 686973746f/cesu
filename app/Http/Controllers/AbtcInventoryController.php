96a42b17c93a2d8eb6a2480dd04bfba186f84818
Histo <hihihisto@protonmail.com>
2025-05-07 16:23:32 +0800

    abtc inventory small fix 2

--- app/Http/Controllers/AbtcInventoryController.php
+++ app/Http/Controllers/AbtcInventoryController.php
@@ -518,7 +518,7 @@ class AbtcInventoryController extends Controller
             foreach($doh_stock_list as $m) {
                 $edpm = AbtcInventoryTransaction::where('stock_id', $m->id)
                 ->whereYear('transaction_date', $previous_month->format('Y'))
-                ->whereMonth('transaction_date', $previous_month->format('n'))
+                ->whereMonth('transaction_date', '<=', $previous_month->format('n'))
                 ->latest()
                 ->first();
 
