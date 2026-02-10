175caeae965e5b3e6eac32d2edb27f63f0859acd
Histo <hihihisto@protonmail.com>
2026-02-10 08:48:58 +0800

    route home fix

--- routes/web.php
+++ routes/web.php
@@ -98,7 +98,7 @@ Route::get('/email/verify', function () {
 Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
     $request->fulfill();
 
-    return redirect('/home');
+    return redirect()->route('home');
 })->middleware(['auth', 'signed'])->name('verification.verify');
 
 Route::post('/email/verification-notification', function (Request $request) {
