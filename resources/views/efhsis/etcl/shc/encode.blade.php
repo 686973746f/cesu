05a8aaaf5287199b470d5f6dbec5f2ca1c1475d4
Histo <hihihisto@protonmail.com>
2026-03-02 08:18:52 +0800

    fixedheader fix

--- resources/views/efhsis/etcl/shc/encode.blade.php
+++ resources/views/efhsis/etcl/shc/encode.blade.php
@@ -48,124 +48,133 @@
                     <input type="hidden" name="month" value="{{ $selectDate->month }}">
                     <input type="hidden" name="year" value="{{ $selectDate->year }}">
 
-                    <table class="table table-bordered table-striped">
-                        <thead class="thead-light text-center">
-                            <tr>
-                                <th rowspan="2">Barangay</th>
-                                <th colspan="3">Pregnant women screened for syphilis</th>
-                                <th colspan="3">Pregnant women screened reactive for syphilis</th>
-                                <th colspan="3">Pregnant women treated for syphilis</th>
-                                <th colspan="3">Pregnant women screened for HIV</th>
-                                <th colspan="3">Pregnant women screened reactive for HIV</th>
-                                <th colspan="3">Pregnant women screened for Hepatitis B</th>
-                                <th colspan="3">Pregnant women screened reactive for Hepatitis B</th>
-                            </tr>
-                            <tr>
-                                <th>10-14 years old</th>
-                                <th>15-19 years old</th>
-                                <th>20-49 years old</th>
-                                <th>10-14 years old</th>
-                                <th>15-19 years old</th>
-                                <th>20-49 years old</th>
-                                <th>10-14 years old</th>
-                                <th>15-19 years old</th>
-                                <th>20-49 years old</th>
-                                <th>10-14 years old</th>
-                                <th>15-19 years old</th>
-                                <th>20-49 years old</th>
-                                <th>10-14 years old</th>
-                                <th>15-19 years old</th>
-                                <th>20-49 years old</th>
-                                <th>10-14 years old</th>
-                                <th>15-19 years old</th>
-                                <th>20-49 years old</th>
-                                <th>10-14 years old</th>
-                                <th>15-19 years old</th>
-                                <th>20-49 years old</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach($list as $ind => $l)
-                            @php
-
-                            $d = App\Models\SocialHygieneTcl::where('address_brgy_code', $l->id)
-                            ->where('year', $selectDate->year)
-                            ->where('month', $selectDate->month)
-                            ->first();
-                            @endphp
-                            <input type="hidden" name="brgy_id[]" value="{{ $l->id }}">
-                            <tr>
-                                <td>{{ $l->name }}</td>
-                                <td>
-                                    <input type="number" class="form-control form-control-sm" name="r_preg_syphilis_a[]" value="{{old('r_preg_syphilis_a')[$ind] ?? $d->r_preg_syphilis_a ?? 0}}" min="0" max="999" required>
-                                </td>
-                                <td>
-                                    <input type="number" class="form-control form-control-sm" name="r_preg_syphilis_b[]" value="{{old('r_preg_syphilis_b')[$ind] ?? $d->r_preg_syphilis_b ?? 0}}" min="0" max="999" required>
-                                </td>
-                                <td>
-                                    <input type="number" class="form-control form-control-sm" name="r_preg_syphilis_c[]" value="{{old('r_preg_syphilis_c')[$ind] ?? $d->r_preg_syphilis_c ?? 0}}" min="0" max="999" required>
-                                </td>
-                                <td>
-                                    <input type="number" class="form-control form-control-sm" name="nr_preg_syphilis_a[]" value="{{old('nr_preg_syphilis_a')[$ind] ?? $d->nr_preg_syphilis_a ?? 0}}" min="0" max="999" required>
-                                </td>
-                                <td>
-                                    <input type="number" class="form-control form-control-sm" name="nr_preg_syphilis_b[]" value="{{old('nr_preg_syphilis_b')[$ind] ?? $d->nr_preg_syphilis_b ?? 0}}" min="0" max="999" required>
-                                </td>
-                                <td>
-                                    <input type="number" class="form-control form-control-sm" name="nr_preg_syphilis_c[]" value="{{old('nr_preg_syphilis_c')[$ind] ?? $d->nr_preg_syphilis_c ?? 0}}" min="0" max="999" required>
-                                </td>
-                                <td>
-                                    <input type="number" class="form-control form-control-sm" name="treated_preg_syphilis_a[]" value="{{old('treated_preg_syphilis_a')[$ind] ?? $d->treated_preg_syphilis_a ?? 0}}" min="0" max="999" required>
-                                </td>
-                                <td>
-                                    <input type="number" class="form-control form-control-sm" name="treated_preg_syphilis_b[]" value="{{old('treated_preg_syphilis_b')[$ind] ?? $d->treated_preg_syphilis_b ?? 0}}" min="0" max="999" required>
-                                </td>
-                                <td>
-                                    <input type="number" class="form-control form-control-sm" name="treated_preg_syphilis_c[]" value="{{old('treated_preg_syphilis_c')[$ind] ?? $d->treated_preg_syphilis_c ?? 0}}" min="0" max="999" required>
-                                </td>
-                                <td>
-                                    <input type="number" class="form-control form-control-sm" name="r_preg_hiv_a[]" value="{{old('r_preg_hiv_a')[$ind] ?? $d->r_preg_hiv_a ?? 0}}" min="0" max="999" required>
-                                </td>
-                                <td>
-                                    <input type="number" class="form-control form-control-sm" name="r_preg_hiv_b[]" value="{{old('r_preg_hiv_b')[$ind] ?? $d->r_preg_hiv_b ?? 0}}" min="0" max="999" required>
-                                </td>
-                                <td>
-                                    <input type="number" class="form-control form-control-sm" name="r_preg_hiv_c[]" value="{{old('r_preg_hiv_c')[$ind] ?? $d->r_preg_hiv_c ?? 0}}" min="0" max="999" required>
-                                </td>
-                                <td>
-                                    <input type="number" class="form-control form-control-sm" name="nr_preg_hiv_a[]" value="{{old('nr_preg_hiv_a')[$ind] ?? $d->nr_preg_hiv_a ?? 0}}" min="0" max="999" required>
-                                </td>
-                                <td>
-                                    <input type="number" class="form-control form-control-sm" name="nr_preg_hiv_b[]" value="{{old('nr_preg_hiv_b')[$ind] ?? $d->nr_preg_hiv_b ?? 0}}" min="0" max="999" required>
-                                </td>
-                                <td>
-                                    <input type="number" class="form-control form-control-sm" name="nr_preg_hiv_c[]" value="{{old('nr_preg_hiv_c')[$ind] ?? $d->nr_preg_hiv_c ?? 0}}" min="0" max="999" required>
-                                </td>
-                                <td>
-                                    <input type="number" class="form-control form-control-sm" name="r_preg_hepab_a[]" value="{{old('r_preg_hepab_a')[$ind] ?? $d->r_preg_hepab_a ?? 0}}" min="0" max="999" required>
-                                </td>
-                                <td>
-                                    <input type="number" class="form-control form-control-sm" name="r_preg_hepab_b[]" value="{{old('r_preg_hepab_b')[$ind] ?? $d->r_preg_hepab_b ?? 0}}" min="0" max="999" required>
-                                </td>
-                                <td>
-                                    <input type="number" class="form-control form-control-sm" name="r_preg_hepab_c[]" value="{{old('r_preg_hepab_c')[$ind] ?? $d->r_preg_hepab_c ?? 0}}" min="0" max="999" required>
-                                </td>
-                                <td>
-                                    <input type="number" class="form-control form-control-sm" name="nr_preg_hepab_a[]" value="{{old('nr_preg_hepab_a')[$ind] ?? $d->nr_preg_hepab_a ?? 0}}" min="0" max="999" required>
-                                </td>
-                                <td>
-                                    <input type="number" class="form-control form-control-sm" name="nr_preg_hepab_b[]" value="{{old('nr_preg_hepab_b')[$ind] ?? $d->nr_preg_hepab_b ?? 0}}" min="0" max="999" required>
-                                </td>
-                                <td>
-                                    <input type="number" class="form-control form-control-sm" name="nr_preg_hepab_c[]" value="{{old('nr_preg_hepab_c')[$ind] ?? $d->nr_preg_hepab_c ?? 0}}" min="0" max="999" required>
-                                </td>
-                            </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
+                    <div class="table-responsive">
+                        <table class="table table-bordered table-striped" id="mainTbl">
+                            <thead class="thead-light text-center">
+                                <tr>
+                                    <th rowspan="2">Barangay</th>
+                                    <th colspan="3">Pregnant women screened for syphilis</th>
+                                    <th colspan="3">Pregnant women screened reactive for syphilis</th>
+                                    <th colspan="3">Pregnant women treated for syphilis</th>
+                                    <th colspan="3">Pregnant women screened for HIV</th>
+                                    <th colspan="3">Pregnant women screened reactive for HIV</th>
+                                    <th colspan="3">Pregnant women screened for Hepatitis B</th>
+                                    <th colspan="3">Pregnant women screened reactive for Hepatitis B</th>
+                                </tr>
+                                <tr>
+                                    <th>10-14 years old</th>
+                                    <th>15-19 years old</th>
+                                    <th>20-49 years old</th>
+                                    <th>10-14 years old</th>
+                                    <th>15-19 years old</th>
+                                    <th>20-49 years old</th>
+                                    <th>10-14 years old</th>
+                                    <th>15-19 years old</th>
+                                    <th>20-49 years old</th>
+                                    <th>10-14 years old</th>
+                                    <th>15-19 years old</th>
+                                    <th>20-49 years old</th>
+                                    <th>10-14 years old</th>
+                                    <th>15-19 years old</th>
+                                    <th>20-49 years old</th>
+                                    <th>10-14 years old</th>
+                                    <th>15-19 years old</th>
+                                    <th>20-49 years old</th>
+                                    <th>10-14 years old</th>
+                                    <th>15-19 years old</th>
+                                    <th>20-49 years old</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($list as $ind => $l)
+                                @php
+    
+                                $d = App\Models\SocialHygieneTcl::where('address_brgy_code', $l->id)
+                                ->where('year', $selectDate->year)
+                                ->where('month', $selectDate->month)
+                                ->first();
+                                @endphp
+                                <input type="hidden" name="brgy_id[]" value="{{ $l->id }}">
+                                <tr>
+                                    <td>{{ $l->name }}</td>
+                                    <td>
+                                        <input type="number" class="form-control form-control-sm" name="r_preg_syphilis_a[]" value="{{old('r_preg_syphilis_a')[$ind] ?? $d->r_preg_syphilis_a ?? 0}}" min="0" max="999" required>
+                                    </td>
+                                    <td>
+                                        <input type="number" class="form-control form-control-sm" name="r_preg_syphilis_b[]" value="{{old('r_preg_syphilis_b')[$ind] ?? $d->r_preg_syphilis_b ?? 0}}" min="0" max="999" required>
+                                    </td>
+                                    <td>
+                                        <input type="number" class="form-control form-control-sm" name="r_preg_syphilis_c[]" value="{{old('r_preg_syphilis_c')[$ind] ?? $d->r_preg_syphilis_c ?? 0}}" min="0" max="999" required>
+                                    </td>
+                                    <td>
+                                        <input type="number" class="form-control form-control-sm" name="nr_preg_syphilis_a[]" value="{{old('nr_preg_syphilis_a')[$ind] ?? $d->nr_preg_syphilis_a ?? 0}}" min="0" max="999" required>
+                                    </td>
+                                    <td>
+                                        <input type="number" class="form-control form-control-sm" name="nr_preg_syphilis_b[]" value="{{old('nr_preg_syphilis_b')[$ind] ?? $d->nr_preg_syphilis_b ?? 0}}" min="0" max="999" required>
+                                    </td>
+                                    <td>
+                                        <input type="number" class="form-control form-control-sm" name="nr_preg_syphilis_c[]" value="{{old('nr_preg_syphilis_c')[$ind] ?? $d->nr_preg_syphilis_c ?? 0}}" min="0" max="999" required>
+                                    </td>
+                                    <td>
+                                        <input type="number" class="form-control form-control-sm" name="treated_preg_syphilis_a[]" value="{{old('treated_preg_syphilis_a')[$ind] ?? $d->treated_preg_syphilis_a ?? 0}}" min="0" max="999" required>
+                                    </td>
+                                    <td>
+                                        <input type="number" class="form-control form-control-sm" name="treated_preg_syphilis_b[]" value="{{old('treated_preg_syphilis_b')[$ind] ?? $d->treated_preg_syphilis_b ?? 0}}" min="0" max="999" required>
+                                    </td>
+                                    <td>
+                                        <input type="number" class="form-control form-control-sm" name="treated_preg_syphilis_c[]" value="{{old('treated_preg_syphilis_c')[$ind] ?? $d->treated_preg_syphilis_c ?? 0}}" min="0" max="999" required>
+                                    </td>
+                                    <td>
+                                        <input type="number" class="form-control form-control-sm" name="r_preg_hiv_a[]" value="{{old('r_preg_hiv_a')[$ind] ?? $d->r_preg_hiv_a ?? 0}}" min="0" max="999" required>
+                                    </td>
+                                    <td>
+                                        <input type="number" class="form-control form-control-sm" name="r_preg_hiv_b[]" value="{{old('r_preg_hiv_b')[$ind] ?? $d->r_preg_hiv_b ?? 0}}" min="0" max="999" required>
+                                    </td>
+                                    <td>
+                                        <input type="number" class="form-control form-control-sm" name="r_preg_hiv_c[]" value="{{old('r_preg_hiv_c')[$ind] ?? $d->r_preg_hiv_c ?? 0}}" min="0" max="999" required>
+                                    </td>
+                                    <td>
+                                        <input type="number" class="form-control form-control-sm" name="nr_preg_hiv_a[]" value="{{old('nr_preg_hiv_a')[$ind] ?? $d->nr_preg_hiv_a ?? 0}}" min="0" max="999" required>
+                                    </td>
+                                    <td>
+                                        <input type="number" class="form-control form-control-sm" name="nr_preg_hiv_b[]" value="{{old('nr_preg_hiv_b')[$ind] ?? $d->nr_preg_hiv_b ?? 0}}" min="0" max="999" required>
+                                    </td>
+                                    <td>
+                                        <input type="number" class="form-control form-control-sm" name="nr_preg_hiv_c[]" value="{{old('nr_preg_hiv_c')[$ind] ?? $d->nr_preg_hiv_c ?? 0}}" min="0" max="999" required>
+                                    </td>
+                                    <td>
+                                        <input type="number" class="form-control form-control-sm" name="r_preg_hepab_a[]" value="{{old('r_preg_hepab_a')[$ind] ?? $d->r_preg_hepab_a ?? 0}}" min="0" max="999" required>
+                                    </td>
+                                    <td>
+                                        <input type="number" class="form-control form-control-sm" name="r_preg_hepab_b[]" value="{{old('r_preg_hepab_b')[$ind] ?? $d->r_preg_hepab_b ?? 0}}" min="0" max="999" required>
+                                    </td>
+                                    <td>
+                                        <input type="number" class="form-control form-control-sm" name="r_preg_hepab_c[]" value="{{old('r_preg_hepab_c')[$ind] ?? $d->r_preg_hepab_c ?? 0}}" min="0" max="999" required>
+                                    </td>
+                                    <td>
+                                        <input type="number" class="form-control form-control-sm" name="nr_preg_hepab_a[]" value="{{old('nr_preg_hepab_a')[$ind] ?? $d->nr_preg_hepab_a ?? 0}}" min="0" max="999" required>
+                                    </td>
+                                    <td>
+                                        <input type="number" class="form-control form-control-sm" name="nr_preg_hepab_b[]" value="{{old('nr_preg_hepab_b')[$ind] ?? $d->nr_preg_hepab_b ?? 0}}" min="0" max="999" required>
+                                    </td>
+                                    <td>
+                                        <input type="number" class="form-control form-control-sm" name="nr_preg_hepab_c[]" value="{{old('nr_preg_hepab_c')[$ind] ?? $d->nr_preg_hepab_c ?? 0}}" min="0" max="999" required>
+                                    </td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
                     <button type="submit" class="btn btn-success btn-block">Submit</button>
                 </form>
             </div>
         </div>
     </div>
+
+    <script>
+        $('#mainTbl').dataTable({
+            "lengthMenu": [[-1], ["All"]],
+            fixedHeader: true,
+        });
+    </script>
 @endsection
