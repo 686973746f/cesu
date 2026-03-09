9209170965ac04b3c3692c8534de20ec188b9ad4
Histo <hihihisto@protonmail.com>
2026-03-09 11:32:55 +0800

    lab page fix

--- resources/views/pidsr/laboratory/home.blade.php
+++ resources/views/pidsr/laboratory/home.blade.php
@@ -41,6 +41,10 @@
                         @endforeach
                     </tbody>
                 </table>
+
+                <div class="pagination justify-content-center mt-3">
+                    {{$list->appends(request()->input())->links()}}
+                </div>
             </div>
         </div>
     </div>
